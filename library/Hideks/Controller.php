ff2cc05f84919e40761bcc2fec34ba9c383908f7
Hideks <hideksys@gmail.com>
2014-03-26 12:46:24 -0300

    Fix #3 - Adicionado o segundo parâmetro da função renderTo

--- library/Hideks/Controller.php
+++ library/Hideks/Controller.php
@@ -18,56 +18,60 @@ class Controller {
         $this->_smarty = $frontController->getSmarty();
     }
     
-    public function renderTo($output) {
-        switch($output){
-            case 'html':
-                header('Content-Type: text/html; charset=utf-8');
-                
-                $controller = strtolower( $this->_request->getController() );
-                
-                $action = str_replace("Action", "", $this->_request->getAction());
-                
-                $view = "{$controller}/{$action}.phtml";
-                
-                $view   = (isset($options['view']))     ? $options['view']      : $view;
-                $layout = (isset($options['layout']))   ? $options['layout']    : "layout";
-                
-                if(!file_exists(APPLICATION_PATH.DS.'views'.DS.'layouts'.DS.$layout.'.phtml')){
-                    throw new \Exception($layout.' layout not found!!');
-                }
-                
-                if(!file_exists(APPLICATION_PATH.DS.'views'.DS.$view)){
-                    throw new \Exception($view.' view not found!!');
-                }
-                
-                $this->_smarty->display('extends:layouts'.DS.$layout.'.phtml|'.$view);
-                break;
-            case 'json':
-                $options = empty($options) ? true : $options; 
-                
-                echo json_encode($options);
-                exit();
-                break;
-            case 'xml':
-                header("Content-Type: application/xml; charset=UTF-8");
-                
-                echo '<?xml version="1.0" encoding="UTF-8"?>';
-                echo '<?xml-stylesheet type="text/css" href="/stylesheets/xml.css"?>';
-                
-                $controller = strtolower( $this->_request->getController() );
-                
-                $action = str_replace("Action", "", $this->_request->getAction());
-                
-                $view = "{$controller}/{$action}.phtml";
-                
-                $this->smarty->display($view);
-                break;
-            default:
-                throw new \Exception('The output '.$output.' is not supported!!');
-                break;
+    public function renderTo($output, $options = null) {
+        if( !method_exists($this, $output) ){
+            throw new \Exception('The output '.$output.' is not supported!!');
         }
+        
+        call_user_func_array(array($this, $output), array($options));
     }
     
+    private function html($options) {
+        header('Content-Type: text/html; charset=utf-8');
+
+        $controller = strtolower( $this->_request->getController() );
+
+        $action = str_replace("Action", "", $this->_request->getAction());
+
+        $view = "{$controller}/{$action}.phtml";
+
+        $view   = (isset($options['view']))     ? $options['view']      : $view;
+        $layout = (isset($options['layout']))   ? $options['layout']    : "layout";
+
+        if(!file_exists(APPLICATION_PATH.DS.'views'.DS.'layouts'.DS.$layout.'.phtml')){
+            throw new \Exception($layout.' layout not found!!');
+        }
+
+        if(!file_exists(APPLICATION_PATH.DS.'views'.DS.$view)){
+            throw new \Exception($view.' view not found!!');
+        }
+
+        $this->_smarty->display('extends:layouts'.DS.$layout.'.phtml|'.$view);
+    }
+    
+    private function json($options) {
+        header('Content-Type: application/json; charset=utf-8');
+        
+        $options = empty($options) ? true : $options; 
+
+        echo json_encode($options);
+    }
+    
+    private function xml() {
+        header("Content-Type: application/xml; charset=utf-8");
+
+        echo '<?xml version="1.0" encoding="utf-8" ?>';
+        echo '<?xml-stylesheet type="text/css" href="/stylesheets/xml.css" ?>';
+
+        $controller = strtolower( $this->_request->getController() );
+
+        $action = str_replace("Action", "", $this->_request->getAction());
+
+        $view = "{$controller}/{$action}.phtml";
+
+        $this->_smarty->display($view);
+    }
+
     public function linkTo($routeName) {
         return $this->_router->_linkTo($routeName);
     }

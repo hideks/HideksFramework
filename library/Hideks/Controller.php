cde9f78d748f0f2022f1ef4224dbef0e43b337d0
Hideks <hideksys@gmail.com>
2014-04-17 18:54:13 -0300

    Classe Controller refatorada

--- library/Hideks/Controller.php
+++ library/Hideks/Controller.php
@@ -18,9 +18,29 @@ class Controller {
         $this->_smarty = $frontController->getSmarty();
     }
     
+    public function getRequest() {
+        return $this->_request;
+    }
+    
+    public function getSmarty() {
+        return $this->_smarty;
+    }
+    
+    public function getParam($param) {
+        return $this->_request->_getParams($param);
+    }
+    
+    public function linkTo($routeName, array $params = array()) {
+        if( !$this->_router->getRoutes()->exists($routeName) ){
+            throw new \Exception("No route with the name $routeName has been found!!");
+        }
+        
+        return $this->_router->linkTo($routeName, $params);
+    }
+    
     public function renderTo($output, $options = null) {
         if( !method_exists($this, $output) ){
-            throw new \Exception('The output '.$output.' is not supported!!');
+            throw new \Exception('The output: '.$output.' is not supported!!');
         }
         
         call_user_func_array(array($this, $output), array($options));
@@ -31,57 +51,45 @@ class Controller {
 
         $controller = strtolower( $this->_request->getController() );
 
-        $action = str_replace("Action", "", $this->_request->getAction());
+        $action = str_replace('Action', '', $this->_request->getAction());
 
-        $view = "{$controller}/{$action}.phtml";
+        $view = $controller.DS.$action;
 
         $view   = (isset($options['view']))     ? $options['view']      : $view;
-        $layout = (isset($options['layout']))   ? $options['layout']    : "layout";
+        $layout = (isset($options['layout']))   ? $options['layout']    : 'layout';
 
         if(!file_exists(APPLICATION_PATH.DS.'views'.DS.'layouts'.DS.$layout.'.phtml')){
             throw new \Exception($layout.' layout not found!!');
         }
 
-        if(!file_exists(APPLICATION_PATH.DS.'views'.DS.$view)){
+        if(!file_exists(APPLICATION_PATH.DS.'views'.DS.$view.'.phtml')){
             throw new \Exception($view.' view not found!!');
         }
-
-        $this->_smarty->display('extends:layouts'.DS.$layout.'.phtml|'.$view);
+        
+        $this->_smarty->display('extends:layouts'.DS.$layout.'.phtml|'.$view.'.phtml');
     }
     
     private function json($options) {
         header('Content-Type: application/json; charset=utf-8');
         
-        $options = empty($options) ? true : $options; 
+        $options = is_null($options) ? true : $options; 
 
         echo json_encode($options);
     }
     
     private function xml() {
-        header("Content-Type: application/xml; charset=utf-8");
+        header('Content-Type: application/xml; charset=utf-8');
 
         echo '<?xml version="1.0" encoding="utf-8" ?>';
         echo '<?xml-stylesheet type="text/css" href="/stylesheets/xml.css" ?>';
 
         $controller = strtolower( $this->_request->getController() );
 
-        $action = str_replace("Action", "", $this->_request->getAction());
+        $action = str_replace('Action', '', $this->_request->getAction());
 
-        $view = "{$controller}/{$action}.phtml";
+        $view = $controller.DS.$action.'.phtml';
 
         $this->_smarty->display($view);
-    }
-
-    public function linkTo($routeName) {
-        return $this->_router->_linkTo($routeName);
-    }
-    
-    public function getParam($param) {
-        return $this->_request->_getParams($param);
-    }
-    
-    public function getSmarty() {
-        return $this->_smarty;
     }
     
 }

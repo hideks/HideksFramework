614fc1a5841e70da6882d4c3b0827da67ee58d3a
Hideks <hideksys@gmail.com>
2014-04-25 17:29:45 -0300

    Atualizado Controller.php

--- library/Hideks/Controller.php
+++ library/Hideks/Controller.php
@@ -47,57 +47,53 @@ class Controller {
             throw new \Exception('Unable to write to <span class="glyphicon glyphicon-home"></span> /application/temp/compiled/');
         }
         
-        if( !method_exists($this, $output) ){
-            throw new \Exception('The output: '.$output.' is not supported!!');
-        }
+        switch($output){
+            case 'html':
+                header('Content-Type: text/html; charset=utf-8');
+
+                $controller = strtolower( $this->_request->getController() );
+
+                $action = str_replace('Action', '', $this->_request->getAction());
+
+                $view = $controller.DS.$action;
+
+                $view   = (isset($options['view']))     ? $options['view']      : $view;
+                $layout = (isset($options['layout']))   ? $options['layout']    : 'layout';
+
+                if( !file_exists(APPLICATION_PATH.DS.'views'.DS.'layouts'.DS.$layout.'.phtml') ){
+                    throw new \Exception($layout.' layout not found!!');
+                }
+
+                if( !file_exists(APPLICATION_PATH.DS.'views'.DS.$view.'.phtml') ){
+                    throw new \Exception($view.' view not found!!');
+                }
+
+                $this->_smarty->display('extends:layouts'.DS.$layout.'.phtml|'.$view.'.phtml');
+                break;
+            case 'json':
+                header('Content-Type: application/json; charset=utf-8');
         
-        call_user_func_array(array($this, $output), array($options));
-    }
-    
-    private function html($options) {
-        header('Content-Type: text/html; charset=utf-8');
+                $options = is_null($options) ? true : $options; 
 
-        $controller = strtolower( $this->_request->getController() );
+                echo json_encode($options);
+                break;
+            case 'xml':
+                header('Content-Type: application/xml; charset=utf-8');
 
-        $action = str_replace('Action', '', $this->_request->getAction());
+                echo '<?xml version="1.0" encoding="utf-8" ?>';
+                echo '<?xml-stylesheet type="text/css" href="/stylesheets/xml.css" ?>';
 
-        $view = $controller.DS.$action;
+                $controller = strtolower( $this->_request->getController() );
 
-        $view   = (isset($options['view']))     ? $options['view']      : $view;
-        $layout = (isset($options['layout']))   ? $options['layout']    : 'layout';
+                $action = str_replace('Action', '', $this->_request->getAction());
 
-        if( !file_exists(APPLICATION_PATH.DS.'views'.DS.'layouts'.DS.$layout.'.phtml') ){
-            throw new \Exception($layout.' layout not found!!');
+                $view = $controller.DS.$action.'.phtml';
+
+                $this->_smarty->display($view);
+                break;
+            default:
+                throw new \Exception('The output: '.$output.' is not supported!!');
         }
-
-        if( !file_exists(APPLICATION_PATH.DS.'views'.DS.$view.'.phtml') ){
-            throw new \Exception($view.' view not found!!');
-        }
-        
-        $this->_smarty->display('extends:layouts'.DS.$layout.'.phtml|'.$view.'.phtml');
-    }
-    
-    private function json($options) {
-        header('Content-Type: application/json; charset=utf-8');
-        
-        $options = is_null($options) ? true : $options; 
-
-        echo json_encode($options);
-    }
-    
-    private function xml() {
-        header('Content-Type: application/xml; charset=utf-8');
-
-        echo '<?xml version="1.0" encoding="utf-8" ?>';
-        echo '<?xml-stylesheet type="text/css" href="/stylesheets/xml.css" ?>';
-
-        $controller = strtolower( $this->_request->getController() );
-
-        $action = str_replace('Action', '', $this->_request->getAction());
-
-        $view = $controller.DS.$action.'.phtml';
-
-        $this->_smarty->display($view);
     }
     
 }

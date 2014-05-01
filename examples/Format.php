23955751b8bc75284215f99eef68ffb20c7754d5
Hideks <hideksys@gmail.com>
2014-05-01 14:43:40 -0300

    Atualizado Format.php

--- examples/Format.php
+++ examples/Format.php
@@ -3,19 +3,16 @@
 class Format extends \Hideks\Controller {
     
     public function indexAction() {
-        /* Formatando a string para o seo - Início */
+        // Formatando a string para o seo
         echo \Hideks\Format::string('TeStE dE StRiNg')->toSeo();
-        /* Formatando a string para o seo - Final */
         
-        /* Formatando a string para o gravar no banco de dados - Início */
+        // Formatando a string para o gravar no banco de dados
         echo \Hideks\Format::string('TeStE dE \'StRiNg')->toPreventInjection();
-        /* Formatando a string para o gravar no banco de dados - Final */
         
-        /* Formatando os objetos para uma string separada por vírgulas - Início */
+        // Formatando os objetos para uma string separada por vírgulas
         $users = Usuario::all();
         
         echo \Hideks\Format::objects($users)->toStringByColumn('us_nome');
-        /* Formatando os objetos para uma string separada por vírgulas - Final */
     }
     
 }
